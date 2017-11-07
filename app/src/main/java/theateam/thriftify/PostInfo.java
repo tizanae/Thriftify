a3a6bd9419d7dc45fc53cd1c296fb38580ae0ef8
jcarrillo1 <xjcarrillox@gmail.com>
2017-11-07 11:19:16 -0500

    Added carousel to post info page

--- app/src/main/java/theateam/thriftify/PostInfo.java
+++ app/src/main/java/theateam/thriftify/PostInfo.java
@@ -2,12 +2,31 @@ package theateam.thriftify;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.synnapps.carouselview.CarouselView;
+import com.synnapps.carouselview.ImageListener;
 
 public class PostInfo extends AppCompatActivity {
+    CarouselView carouselView;
+
+    int[] sampleImages = {R.drawable.post_image_2, R.drawable.post_image_1, R.drawable.post_image_3};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post_info);
+
+        carouselView = findViewById(R.id.carouselView);
+        carouselView.setPageCount(sampleImages.length);
+
+        carouselView.setImageListener(imageListener);
     }
+
+    ImageListener imageListener = new ImageListener() {
+        @Override
+        public void setImageForPosition(int position, ImageView imageView) {
+            imageView.setImageResource(sampleImages[position]);
+        }
+    };
 }
