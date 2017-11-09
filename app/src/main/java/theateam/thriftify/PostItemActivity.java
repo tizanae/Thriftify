068cc2884f1ce3a31a14f1ba7ab36329f3390668
jcarrillo1 <xjcarrillox@gmail.com>
2017-11-09 02:02:44 -0500

    Updated adapter to use arrayadapter, now works with listview

--- app/src/main/java/theateam/thriftify/PostItemActivity.java
+++ app/src/main/java/theateam/thriftify/PostItemActivity.java
@@ -1,7 +1,12 @@
 package theateam.thriftify;
 
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AlertDialog;
@@ -10,9 +15,20 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ListView;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+
+import static android.media.MediaRecorder.VideoSource.CAMERA;
 
 public class PostItemActivity extends BaseActivity {
 
+    static final int REQUEST_IMAGE_GET = 1;
+    static final int REQUEST_IMAGE_CAPTURE = 2;
+    PostItemPictureAdapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,7 +43,14 @@ public class PostItemActivity extends BaseActivity {
                 showDialog();
             }
         });
+        ArrayList<Bitmap> bitmaps = new ArrayList<>();
+        adapter = new PostItemPictureAdapter(this, bitmaps);
+        adapter.setNotifyOnChange(true);
+        ListView listView = (ListView) findViewById(R.id.picturesContainer);
+        listView.setAdapter(adapter);
+
     }
+
     private void showDialog(){
         AlertDialog.Builder dialog = new AlertDialog.Builder(this);
         String[] dialogItems = {
@@ -39,11 +62,51 @@ public class PostItemActivity extends BaseActivity {
                 new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
-
+                        switch(which) {
+                            case 0:
+                                selectImage();
+                                break;
+                            case 1:
+                                takePicture();
+                                break;
+                        }
                     }
                 }
         );
         dialog.show();
+    }
+
+    public void selectImage() {
+        Intent photoPickerIntent = new Intent(Intent.ACTION_GET_CONTENT);
+        photoPickerIntent.setType("image/*");
+        startActivityForResult(photoPickerIntent, REQUEST_IMAGE_GET);
+    }
+
+    public void takePicture() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap bitmap = (Bitmap) extras.get("data");
+            adapter.add(bitmap);
+        }
+        if (requestCode == REQUEST_IMAGE_GET && resultCode == RESULT_OK) {
+            Uri filePath = data.getData();
+            try {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), filePath);
+                adapter.add(bitmap);
+            } catch (IOException e) {
+
+            }
+
+        }
     }
 
 }
--- app/src/main/java/theateam/thriftify/PostItemPictureAdapter.java
+++ app/src/main/java/theateam/thriftify/PostItemPictureAdapter.java
@@ -2,58 +2,97 @@ package theateam.thriftify;
 
 import android.content.Context;
 import android.graphics.Bitmap;
-import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ImageView;
 
 
-import java.util.List;
+import java.util.ArrayList;
 
 
-/**
- * Created by oflyingturtleo on 10/20/17.
- */
+///**
+// * Created by oflyingturtleo on 10/20/17.
+// */
+//
+//public class PostItemPictureAdapter extends RecyclerView.Adapter<PostItemPictureAdapter.ViewHolder> {
+//    private Context mContext;
+//    private List<Bitmap> pictures;
+//    public class ViewHolder extends RecyclerView.ViewHolder {
+//
+//        public ImageView thumbnail;
+//
+//        public ViewHolder(View view) {
+//            super(view);
+//            thumbnail = view.findViewById(R.id.thumbnail);
+//        }
+//    }
+//
+//
+//
+//
+//    public PostItemPictureAdapter(Context mContext, List<Bitmap> pictures) {
+//        this.mContext = mContext;
+//        this.pictures = pictures;
+//    }
+//
+//    @Override
+//    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+//        View itemView = LayoutInflater.from(parent.getContext())
+//                .inflate(R.layout.post_item_picture, parent, false);
+//
+//        return new ViewHolder(itemView);
+//    }
+//
+//    @Override
+//    public void onBindViewHolder(final ViewHolder holder, int position) {
+//        Bitmap bitmap = pictures.get(position);
+//        holder.thumbnail.setImageBitmap(bitmap);
+//    }
+//
+//    @Override
+//    public int getItemCount() {
+//        return pictures.size();
+//    }
+//}
+public class PostItemPictureAdapter extends ArrayAdapter<Bitmap> {
+    // View lookup cache
+    private static class ViewHolder {
+        ImageView imageView;
+    }
 
-public class PostItemPictureAdapter extends RecyclerView.Adapter<PostItemPictureAdapter.ViewHolder> {
-    private Context mContext;
-    private List<Bitmap> pictures;
-    public class ViewHolder extends RecyclerView.ViewHolder {
+    public PostItemPictureAdapter(Context context, ArrayList<Bitmap> bitmaps) {
+        super(context, R.layout.post_item_picture, bitmaps);
+    }
 
-        public ImageView thumbnail;
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        final Bitmap bitmap = getItem(position);
+        ViewHolder viewHolder;
 
-        public ViewHolder(View view) {
-            super(view);
-            thumbnail = view.findViewById(R.id.thumbnail);
+        if (convertView == null) {
+            viewHolder = new ViewHolder();
+            LayoutInflater inflater = LayoutInflater.from(getContext());
+            convertView = inflater.inflate(R.layout.post_item_picture, parent, false);
+            viewHolder.imageView = (ImageView) convertView.findViewById(R.id.thumbnail);
+            convertView.setTag(viewHolder);
+        } else {
+            viewHolder = (ViewHolder) convertView.getTag();
         }
-    }
+        Button btnDelete = (Button) convertView.findViewById(R.id.deleteButton);
+        Button btnDefault = (Button) convertView.findViewById(R.id.defaultButton);
+        btnDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                remove(bitmap);
+                notifyDataSetChanged();
+            }
+        });
+        viewHolder.imageView.setImageBitmap(bitmap);
 
-
-
-
-    public PostItemPictureAdapter(Context mContext, List<Bitmap> pictures) {
-        this.mContext = mContext;
-        this.pictures = pictures;
-    }
-
-    @Override
-    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View itemView = LayoutInflater.from(parent.getContext())
-                .inflate(R.layout.post_item_picture, parent, false);
-
-        return new ViewHolder(itemView);
-    }
-
-    @Override
-    public void onBindViewHolder(final ViewHolder holder, int position) {
-        Bitmap bitmap = pictures.get(position);
-        holder.thumbnail.setImageBitmap(bitmap);
-    }
-
-    @Override
-    public int getItemCount() {
-        return pictures.size();
+        return convertView;
     }
 }
 
