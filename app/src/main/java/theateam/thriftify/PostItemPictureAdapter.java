7dfba04e35f9ce880f5c37802728d4def528dc67
jcarrillo1 <xjcarrillox@gmail.com>
2017-11-09 02:17:56 -0500

    Removed old code from the post item picture adapter

--- app/src/main/java/theateam/thriftify/PostItemPictureAdapter.java
+++ app/src/main/java/theateam/thriftify/PostItemPictureAdapter.java
@@ -12,53 +12,7 @@ import android.widget.ImageView;
 
 import java.util.ArrayList;
 
-
-///**
-// * Created by oflyingturtleo on 10/20/17.
-// */
-//
-//public class PostItemPictureAdapter extends RecyclerView.Adapter<PostItemPictureAdapter.ViewHolder> {
-//    private Context mContext;
-//    private List<Bitmap> pictures;
-//    public class ViewHolder extends RecyclerView.ViewHolder {
-//
-//        public ImageView thumbnail;
-//
-//        public ViewHolder(View view) {
-//            super(view);
-//            thumbnail = view.findViewById(R.id.thumbnail);
-//        }
-//    }
-//
-//
-//
-//
-//    public PostItemPictureAdapter(Context mContext, List<Bitmap> pictures) {
-//        this.mContext = mContext;
-//        this.pictures = pictures;
-//    }
-//
-//    @Override
-//    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-//        View itemView = LayoutInflater.from(parent.getContext())
-//                .inflate(R.layout.post_item_picture, parent, false);
-//
-//        return new ViewHolder(itemView);
-//    }
-//
-//    @Override
-//    public void onBindViewHolder(final ViewHolder holder, int position) {
-//        Bitmap bitmap = pictures.get(position);
-//        holder.thumbnail.setImageBitmap(bitmap);
-//    }
-//
-//    @Override
-//    public int getItemCount() {
-//        return pictures.size();
-//    }
-//}
 public class PostItemPictureAdapter extends ArrayAdapter<Bitmap> {
-    // View lookup cache
     private static class ViewHolder {
         ImageView imageView;
     }
@@ -76,13 +30,13 @@ public class PostItemPictureAdapter extends ArrayAdapter<Bitmap> {
             viewHolder = new ViewHolder();
             LayoutInflater inflater = LayoutInflater.from(getContext());
             convertView = inflater.inflate(R.layout.post_item_picture, parent, false);
-            viewHolder.imageView = (ImageView) convertView.findViewById(R.id.thumbnail);
+            viewHolder.imageView = convertView.findViewById(R.id.thumbnail);
             convertView.setTag(viewHolder);
         } else {
             viewHolder = (ViewHolder) convertView.getTag();
         }
-        Button btnDelete = (Button) convertView.findViewById(R.id.deleteButton);
-        Button btnDefault = (Button) convertView.findViewById(R.id.defaultButton);
+        Button btnDelete = convertView.findViewById(R.id.deleteButton);
+//        Button btnDefault = (Button) convertView.findViewById(R.id.defaultButton);
         btnDelete.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
