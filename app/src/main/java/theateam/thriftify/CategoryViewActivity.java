6d1a0d70f0198dcefc464fa72f104a9a60327f8c
jcarrillo1 <xjcarrillox@gmail.com>
2017-11-11 00:57:53 -0500

    Updated main activity

--- app/src/main/java/theateam/thriftify/CategoryViewActivity.java
+++ app/src/main/java/theateam/thriftify/CategoryViewActivity.java
@@ -5,8 +5,18 @@ import android.os.Bundle;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
+import java.util.Map;
 
 public class CategoryViewActivity extends BaseActivity {
 
@@ -17,6 +27,7 @@ public class CategoryViewActivity extends BaseActivity {
 
         getToolbar();
         setBackArrow();
+
 
         RecyclerView itemViewer = (RecyclerView)findViewById(R.id.item_viewer);
         itemViewer.setHasFixedSize(true);
--- app/src/main/java/theateam/thriftify/MainActivity.java
+++ app/src/main/java/theateam/thriftify/MainActivity.java
@@ -4,18 +4,33 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 import com.mikepenz.materialdrawer.Drawer;
 import com.mikepenz.materialdrawer.DrawerBuilder;
 import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
 import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class MainActivity extends BaseActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,34 +39,49 @@ public class MainActivity extends BaseActivity {
         getToolbar();
         getDrawer();
 
-        // Main Stuff
-        String[] values = new String[] {
-                "Popular Nearby",
-                "Electronics",
-                "Tools",
-                "Video Games",
-                "Antiques",
-                "Appliances",
-                "Arts",
-                "Auto Parts"
-        };
-        ArrayAdapter<String> categoryAdapter = new ArrayAdapter<String>(this,
-                R.layout.category_item,
-                R.id.category_name,
-                values
-        );
-        ListView listView = findViewById(R.id.categories);
-        listView.setAdapter(categoryAdapter);
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            public void onItemClick(AdapterView parent, View v, int position, long id) {
-                openCategory();
+        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference();
+
+        Query categoriesQuery = databaseReference.child("categories").orderByKey();
+
+        categoriesQuery.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                final HashMap<String, String> categoryKeys = new HashMap<>();
+                final ArrayList<String> categories = new ArrayList<>();
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    String category = snapshot.getValue(String.class);
+                    categoryKeys.put(category, snapshot.getKey());
+                    categories.add(category);
+                }
+                final ArrayAdapter<String> categoryAdapter = new ArrayAdapter<>(
+                        MainActivity.this,
+                        R.layout.category_item,
+                        R.id.category_name,
+                        categories
+                );
+                ListView listView = findViewById(R.id.categories);
+                listView.setAdapter(categoryAdapter);
+                listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    public void onItemClick(AdapterView parent, View v, int position, long id) {
+                        String categoryName = (String)parent.getItemAtPosition(position);
+                        openCategory(categoryKeys, categoryName);
+                    }
+                });
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
             }
         });
 
+
     }
 
-    private void openCategory() {
+    private void openCategory(HashMap<String, String> categoryKeys, String categoryName) {
         Intent intent = new Intent(this, theateam.thriftify.CategoryViewActivity.class);
+        String categoryKey = categoryKeys.get(categoryName);
+        intent.putExtra("CATEGORY_KEY", categoryKey);
         startActivity(intent);
     }
 
